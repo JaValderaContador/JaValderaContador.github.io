129b0bf0fcb5063ce93f95124136bfa0550e6016
JaValderaContador <javaldera@miuandes.cl>
2024-12-17 20:29:59 -0300

    Page reset

--- script.js
+++
@@ -1,5 +0,0 @@
-function addComment(){
-    var inputVal = document.getElementById("NewComment").value;
-    var commentaries = document.getElementById("Comentaries");
-    commentaries.innerHTML = "<p>" + inputVal + "<p>";
-}
--- simple_extra_alert.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    alert('Proceed under your and only your responsability');
-})
